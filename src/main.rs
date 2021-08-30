95b6a4d7268db42d41afbcc5b15c11c27070c36f
osama530 <osamaqamar93@gmail.com>
2021-08-30 23:06:27 +0500

    had a little bug

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,20 @@ use cortex_m_semihosting::hprintln;
 
 #[entry]
 fn main() -> ! {
-      let mut peripherals = stm32f303::Peripherals::take().unwrap();
-      let mut rcc = peripherals.RCC;
+      let mut peripherals = &stm32f303::Peripherals::take().unwrap();
+      let mut rcc = &peripherals.RCC;
 
       let gpioa = &peripherals.GPIOA;
       let gpioe = &peripherals.GPIOE;
+      let tim6 = &peripherals.TIM6;
 
 // enabling the gpioe and gpioa registers
       rcc.ahbenr.write(|w| w
             .iopaen().set_bit()
             .iopeen().set_bit()
       );
+// powering the tim6 timer
+      rcc.apb1enr.write(|w| w.tim6en().set_bit());
 
 // configuring pin input output configuration
       gpioe.moder.write(|w| w
@@ -34,9 +37,17 @@ fn main() -> ! {
             .moder13().bits(01)
             .moder14().bits(01)
             .moder15().bits(01)
-
       );
+// configuring tim6 timer
+      tim6.cr1.write(|w| w
+            .opm().set_bit()
+            .cen().clear_bit() );
 
+//configuring prescaler to oprate at 1KHz for 1ms
+      tim6.psc.write(|w| w
+            .psc().bits(7_992)   );
+
+//configuring gpio pins
       gpioa.moder.write(|w| w
             .moder0().bits(00)
       );
@@ -59,16 +70,23 @@ loop {
             8 => { gpioe.bsrr.write(|w| w.bs8().set_bit());
                   asm::delay(4_000_000); },
             9 =>{ gpioe.bsrr.write(|w| w.bs9().set_bit());
-            asm::delay(4_000_000); },
+            delay(peripherals, 1000);      },
+            // asm::delay(4_000_000); },
 
             10 =>{ gpioe.bsrr.write(|w| w.bs10().set_bit());
-            asm::delay(4_000_000); },
+            // asm::delay(4_000_000);
+            delay(peripherals, 1000);
+             },
 
             11 =>{ gpioe.bsrr.write(|w| w.bs11().set_bit());
-            asm::delay(4_000_000); },
+            // asm::delay(4_000_000);
+            delay(peripherals, 1000);
+             },
 
             12 =>{ gpioe.bsrr.write(|w| w.bs12().set_bit());
-            asm::delay(4_000_000); },
+            // asm::delay(4_000_000);
+            delay(peripherals, 1000);
+             },
 
             13 =>{ gpioe.bsrr.write(|w| w.bs13().set_bit());
             asm::delay(4_000_000); },
@@ -96,4 +114,17 @@ loop {
             
             }
       }
+}
+
+fn delay(Tim6: &stm32f303::Peripherals, ms: u16 ) {
+      let tim6 = &Tim6.TIM6;
+
+      tim6.arr.write(|w| w
+            .arr().bits(ms));
+
+      tim6.cr1.write(|w| w
+            .cen().set_bit() );
+      while !tim6.sr.read().uif().bit_is_set() {}
+      tim6.sr.write(|w| w.uif().clear_bit());
+
 }
