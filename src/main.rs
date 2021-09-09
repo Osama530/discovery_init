5e383398401d71f9357a7bd9a2df4797f6b2ad3a
osama530 <osamaqamar93@gmail.com>
2021-09-09 14:09:07 +0500

    programing completed butnot  working as expected

--- src/main.rs
+++ src/main.rs
@@ -12,119 +12,50 @@ use cortex_m_semihosting::hprintln;
 
 #[entry]
 fn main() -> ! {
+
       let mut peripherals = &stm32f303::Peripherals::take().unwrap();
       let mut rcc = &peripherals.RCC;
-
       let gpioa = &peripherals.GPIOA;
-      let gpioe = &peripherals.GPIOE;
-      let tim6 = &peripherals.TIM6;
 
-// enabling the gpioe and gpioa registers
-      rcc.ahbenr.write(|w| w
-            .iopaen().set_bit()
-            .iopeen().set_bit()
-      );
-// powering the tim6 timer
-      rcc.apb1enr.write(|w| w.tim6en().set_bit());
+      let usart1 = &peripherals.USART1;
 
-// configuring pin input output configuration
-      gpioe.moder.write(|w| w
-            .moder8().bits(01)
-            .moder9().bits(01)
-            .moder10().bits(01)
-            .moder11().bits(01)
-            .moder12().bits(01)
-            .moder13().bits(01)
-            .moder14().bits(01)
-            .moder15().bits(01)
-      );
-// configuring tim6 timer
-      tim6.cr1.write(|w| w
-            .opm().set_bit()
-            .cen().clear_bit() );
+//selecting and enabling clock for usart1
+      rcc.cfgr3.write(|w| w.usart1sw().bits(01)); //slecting clock source as pc_)clock (deefault)
+      rcc.apb2enr.write(|w| w.usart1en().set_bit());
+//enabling port and pins assign for usart1
+      rcc.ahbenr.write(|w| w.iopaen().set_bit());
+   
+      gpioa.moder.write(|w| w.moder9().bits(10));     // 00 => input, 01 => output, 10 => Alternate function
+      gpioa.moder.write(|w| w.moder10().bits(10));
+   
+      gpioa.afrh.write(|w| w.afrh9().af7());    //selecting corrosponding alternate function register
+      gpioa.afrh.write(|w| w.afrh10().af7());
 
-//configuring prescaler to oprate at 1KHz for 1ms
-      tim6.psc.write(|w| w
-            .psc().bits(7_992)   );
+      gpioa.pupdr.write(|w| unsafe {w.pupdr9().bits(00)});
+      gpioa.pupdr.write(|w| unsafe {w.pupdr10().bits(00)});
+// defining the word length
+      usart1.cr1.write(|w| w.m().bit8());
+// seting baud rate for usart1
+      usart1.cr1.write(|w| w.over8().set_bit());
+      usart1.brr.write(|w| w.brr().bits(0x47));
+// setting numbers of stop bits
+      usart1.cr2.write(|w| w.stop().bits(00));
+// enableing usart1 and transmission
+      usart1.cr1.write(|w|
+            w
+            .ue().set_bit()
+            .te().set_bit());
+// wait till transmit data register empty
+      while !usart1.isr.read().txe().bit_is_set() {}
+// send data
+      usart1.tdr.write(|w| w.tdr().bits(u16::from(b'X')));
 
-//configuring gpio pins
-      gpioa.moder.write(|w| w
-            .moder0().bits(00)
-      );
       
-      gpioa.pupdr.write(|w| unsafe {
-      w
-            .pupdr0().bits(00)
-      });
-
-
-let mut count = 7;
-
-loop {
       
-      let button_state = gpioa.idr.read().idr0();
-      if button_state == true {
-      count += 1; }
-
-      match count {
-            8 => { gpioe.bsrr.write(|w| w.bs8().set_bit());
-                  asm::delay(4_000_000); },
-            9 =>{ gpioe.bsrr.write(|w| w.bs9().set_bit());
-            delay(peripherals, 1000);      },
-            // asm::delay(4_000_000); },
-
-            10 =>{ gpioe.bsrr.write(|w| w.bs10().set_bit());
-            // asm::delay(4_000_000);
-            delay(peripherals, 1000);
-             },
-
-            11 =>{ gpioe.bsrr.write(|w| w.bs11().set_bit());
-            // asm::delay(4_000_000);
-            delay(peripherals, 1000);
-             },
-
-            12 =>{ gpioe.bsrr.write(|w| w.bs12().set_bit());
-            // asm::delay(4_000_000);
-            delay(peripherals, 1000);
-             },
-
-            13 =>{ gpioe.bsrr.write(|w| w.bs13().set_bit());
-            asm::delay(4_000_000); },
-
-            14 =>{ gpioe.bsrr.write(|w| w.bs14().set_bit());
-            asm::delay(4_000_000); },
-
-            15 =>{ gpioe.bsrr.write(|w| w.bs15().set_bit());
-            asm::delay(4_000_000); },
-
-            _ => { if count >= 15 {
-                  count = 7;
-                  gpioe.bsrr.write(|w| w
-                        .br8().set_bit()
-                        .br9().set_bit()
-                        .br10().set_bit()
-                        .br11().set_bit()
-                        .br12().set_bit()
-                        .br13().set_bit()
-                        .br14().set_bit()
-                        .br15().set_bit())
-                  
-            }
-       }
-            
-            }
-      }
+      loop {
+      while usart1.isr.read().tc().bit_is_clear() {}
+      let txe_status = usart1.isr.read().txe().bits(); 
+      let value = usart1.tdr.read().tdr().bits() as u8;
+      hprintln!("tdr value = {} and txe status = {}", value, txe_status);
 }
-
-fn delay(Tim6: &stm32f303::Peripherals, ms: u16 ) {
-      let tim6 = &Tim6.TIM6;
-
-      tim6.arr.write(|w| w
-            .arr().bits(ms));
-
-      tim6.cr1.write(|w| w
-            .cen().set_bit() );
-      while !tim6.sr.read().uif().bit_is_set() {}
-      tim6.sr.write(|w| w.uif().clear_bit());
-
 }
