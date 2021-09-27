2cd8f1aaef7db1839732fac997efad2801ded323
osama530 <osamaqamar93@gmail.com>
2021-09-27 21:48:54 +0500

    transmit in not working as expected

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ fn main() -> ! {
       let mut peripherals = &stm32f303::Peripherals::take().unwrap();
       let mut rcc = &peripherals.RCC;
       let gpioa = &peripherals.GPIOA;
-
       let usart1 = &peripherals.USART1;
 
 //selecting and enabling clock for usart1
@@ -26,7 +25,12 @@ fn main() -> ! {
       rcc.ahbenr.write(|w| w.iopaen().set_bit());
    
       gpioa.moder.write(|w| w.moder9().bits(10));     // 00 => input, 01 => output, 10 => Alternate function
+      gpioa.otyper.write(|w| w.ot9().clear_bit());          //pa10 = rx,  pa9 = tx
+      gpioa.ospeedr.write(|w| unsafe {w.ospeedr9().bits(01)});
+      
       gpioa.moder.write(|w| w.moder10().bits(10));
+      gpioa.otyper.write(|w| w.ot10().clear_bit());
+      gpioa.ospeedr.write(|w| unsafe {w.ospeedr10().bits(01)});
    
       gpioa.afrh.write(|w| w.afrh9().af7());    //selecting corrosponding alternate function register
       gpioa.afrh.write(|w| w.afrh10().af7());
@@ -40,22 +44,35 @@ fn main() -> ! {
       usart1.brr.write(|w| w.brr().bits(0x47));
 // setting numbers of stop bits
       usart1.cr2.write(|w| w.stop().bits(00));
-// enableing usart1 and transmission
+// enabling usart1 and transmission
       usart1.cr1.write(|w|
             w
             .ue().set_bit()
-            .te().set_bit());
-// wait till transmit data register empty
-      while !usart1.isr.read().txe().bit_is_set() {}
+            .te().set_bit()
+            .re().set_bit());
 // send data
-      usart1.tdr.write(|w| w.tdr().bits(u16::from(b'X')));
+while usart1.isr.read().txe().bit_is_clear() {}
+      // Send a string
+    for byte in b"The quick brown fox jumps over the lazy dog.".iter() {
+      usart1
+          .tdr
+          .write(|w| w.tdr().bits(u16::from(*byte)));
+  }
+// wait till transmit complete and tdr register empty
 
-      
-      
+//receive data
       loop {
-      while usart1.isr.read().tc().bit_is_clear() {}
-      let txe_status = usart1.isr.read().txe().bits(); 
-      let value = usart1.tdr.read().tdr().bits() as u8;
-      hprintln!("tdr value = {} and txe status = {}", value, txe_status);
-}
+
+      while usart1.isr.read().txe().bit_is_clear() {}
+            // Send a string
+          for byte in b"The quick brown fox jumps over the lazy dog.".iter() {
+            usart1
+                .tdr
+                .write(|w| w.tdr().bits(u16::from(*byte)));
+        }
+      
+      while !usart1.isr.read().rxne().bit_is_set() {}
+      let data = usart1.rdr.read().rdr().bits();
+      hprintln!("data recived = {}",data);
+      }
 }
